21cccce47900ab11088160e0ca6ed9820bcabbc1
shanghanrun <idim7@naver.com>
2024-02-16 14:58:36 +0900

    메모.txt와 script2.js를 먼저 살펴보세요.

---
+++ script2.js
@@ -0,0 +1,334 @@
+// 아래처럼 원하는 json파일을 복사해서, data에 할당해서 사용하면 됩니다.
+// 아래에 data 자료를 넣고, 여기 위에서는 코드를 작성하시면 됩니다.
+
+
+
+
+
+
+// 가짜데이터
+const data = {
+    "status": "ok",
+    "totalResults": 34,
+    "articles": [
+        {
+            "source": {
+                "id": "",
+                "name": "Noona Times"
+            },
+            "author": "초코",
+            "title": "여신의 품격: 장원영(Ive) vs 코딩누나(코눈)",
+            "description": "<h5>장원영, 코딩 알려주는 누나와 쌍벽을 이루는 미모</br>'아이즈원'으로 혜성처럼 데뷰해서 꾸준한 인기를 얻어가던 장원영은 새로운 그룹 'Ive'의 리더로 활동하며 글로벌 명성을 이어가고 있다. 한편 코딩계에서 별똥별처럼 반짝이며 등장한 김빛나씨는 미모 뿐만 아니라 뛰어난 실력 및 유머로 챗지피티에게 소문이 날 정도이다. 이 둘의 대결이 어떻게 될 지 흥미진진하다. 코딩누나 살짝 긴장 좀 해야 겠다!! </h5>",
+            "url": "https://cdn.inflearn.com/public/users/thumbnails/694277/60d324e4-719f-4551-8f3c-f377b7eb1f78",
+            "urlToImage": "https://truth.bahamut.com.tw/s01/202209/bb7dd87e8f4d1d0ca3a7d735f873eb38.JPG",
+            "publishedAt": "2024.01.30",
+            "content": ""
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Donga.com"
+            },
+            "author": null,
+            "title": "차은우, 올리비아 핫세 딸과 열애설 해명 “이래도 되는 건가?” - 동아일보",
+            "description": "가수 겸 배우 차은우가 미국 배우 인디아 아이슬리와의 열애설에 대해 해명했다. 아이슬리는 영화 ‘로미오와 줄리엣’(1968)으로 유명한 올리비아 핫세의 딸이다.  차은우는 14일…",
+            "url": "https://www.donga.com/news/Entertainment/article/all/20240215/123529952/1",
+            "urlToImage": "https://dimg.donga.com/a/669/0/95/5/wps/NEWS/IMAGE/2024/02/15/123529670.2.jpg",
+            "publishedAt": "2024-02-15T02:16:00Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Dailymedi.com"
+            },
+            "author": null,
+            "title": "政, 희귀질환·암 조직 등 인체자원 공개 - 데일리메디",
+            "description": null,
+            "url": "https://www.dailymedi.com/news/news_view.php?ca_id=22&amp;wr_id=908330",
+            "urlToImage": null,
+            "publishedAt": "2024-02-15T02:02:00Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Mt.co.kr"
+            },
+            "author": "박정렬",
+            "title": "\"콩·감자 먹어 '이 균' 늘려라\" 화장실 들락날락 '과민성장증후군' 대처법 - 머니투데이",
+            "description": "[박정렬의 신의료인]우리나라 국민의 10명 중 1명이 겪는 과민성장증후군에 '타깃 치료'가 가능한 미생물 균주를 국내 연구진이 발견했다. 분당서울대병원 소화기내과 김나영·이동호 교수 연구팀(최수인 선임연구원, 남령희 연구원)은 동물실험을 통해 과민성장증후군의 치료에 '로즈부리아 파에시스(Roseburia Faecis)'라는 미생물이 효과적이라는 점을 입증했...",
+            "url": "https://news.mt.co.kr/mtview.php?no=2024021510123390806",
+            "urlToImage": "https://thumb.mt.co.kr/21/2024/02/2024021510123390806_1.jpg",
+            "publishedAt": "2024-02-15T01:49:35Z",
+            "content": ""
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Hani.co.kr"
+            },
+            "author": "기민도",
+            "title": "전국 비 또는 눈…낮부터 기온 낮아져 내일 아침 영하 - 한겨레",
+            "description": "15일 전국 대부분 지역에서 비 또는 눈이 내릴 전망이다. 특히 강원 산지에 최대 15㎝ 이상 눈이 내릴 수 있다고 예보됐다. 이날 낮부터는 기온도 차차 낮아져 16∼17일 아침 기온은 영하로 떨어질 것으로 전망된다. 기상청은 이날 새벽 수도권과 강원도, 충청권, 전북",
+            "url": "https://www.hani.co.kr/arti/society/environment/1128421.html",
+            "urlToImage": "https://flexible.img.hani.co.kr/flexible/normal/900/570/imgdb/original/2024/0215/20240215500335.jpg",
+            "publishedAt": "2024-02-15T01:44:00Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "YouTube"
+            },
+            "author": null,
+            "title": "[LIVE] 클린스만 감독 '화상 참석'..축구협회 전력강화위원회, 클린스만 '경질' 논의 / SBS - SBS 뉴스",
+            "description": "Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube.",
+            "url": "https://www.youtube.com/watch?v=O11IVLbsM7A",
+            "urlToImage": null,
+            "publishedAt": "2024-02-15T01:41:11Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Donga.com"
+            },
+            "author": null,
+            "title": "원희룡, 이재명과 맞대결…與, 안철수·방문규 등 25명 단수공천 - 동아일보",
+            "description": "국민의힘 공천관리위원회가 15일 원희룡 전 국토교통부 장관, 안철수 의원, 방문규 전 산업통상자원부 장관, 이수정 경기대 교수 등을 단수 공천하기로 했다. 이번 단수 공천자 명단…",
+            "url": "https://www.donga.com/news/Politics/article/all/20240215/123528786/1",
+            "urlToImage": "https://dimg.donga.com/a/560/0/95/5/wps/NEWS/IMAGE/2024/02/15/123528801.2.jpg",
+            "publishedAt": "2024-02-15T01:34:00Z",
+            "content": ""
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Hani.co.kr"
+            },
+            "author": "임재우",
+            "title": "민주당, 홍익표·고민정 단수공천…24개 지역구 공천심사 발표 - 한겨레",
+            "description": "더불어민주당이 15일 서울 서초을에 홍익표 원내대표, 서울 광진을에 고민정 최고위원을 단수 공천하는 등 24개 선거구에 대한 공천 심사 결과를 발표했다. 민주당 공천관리위원회(위원장 임혁백)는 이날 24개 선거구에 대한 3차 공천 심사 결과를 발표했다. 단수 공천 지역",
+            "url": "https://www.hani.co.kr/arti/politics/politics_general/1128430.html",
+            "urlToImage": "https://flexible.img.hani.co.kr/flexible/normal/800/553/imgdb/original/2024/0202/20240202501081.jpg",
+            "publishedAt": "2024-02-15T01:15:00Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Yonhapnewstv.co.kr"
+            },
+            "author": null,
+            "title": "트럼프 '나토 발언' 나비효과…대선 레이스 쟁점 격화 - 연합뉴스TV",
+            "description": "트럼프 '나토 발언' 나비효과…대선 레이스 쟁점 격화\n\n[앵커]\n\n우크라이나 지원 문제를 둘러싸고 미국 정치권이 힘겨루기가 격화되고 있습니다.\n\n어느 한쪽도 물러설 기미를 보이지",
+            "url": "https://m.yonhapnewstv.co.kr/news/MYH20240215006100032",
+            "urlToImage": "https://yonhapnewstv-prod.s3.ap-northeast-2.amazonaws.com/article/MYH/20240215/MYH20240215006100032_P1.jpg",
+            "publishedAt": "2024-02-15T01:12:20Z",
+            "content": ""
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Chosun.com"
+            },
+            "author": "조선일보",
+            "title": "박나래, '목 수술' 어쩌나? 결국 더빙으로 인사…입원하는데 캐리어 한가득('나혼산') - 조선일보",
+            "description": "박나래, 목 수술 어쩌나 병원 입원하는데 캐리어 한가득",
+            "url": "https://www.chosun.com/entertainments/broadcast/2024/02/15/2TVVLI7ET3V7SHNEWN3MVW3DYU/",
+            "urlToImage": "https://images.chosun.com/resizer/TT_iQSmneHGVztUFAqsrIxZuoAE=/700x367/filters:focal(338x141:348x151)/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/45KOPT45NBPXEL3GLQY6NQJ5GM.jpg",
+            "publishedAt": "2024-02-15T01:02:22Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Kita.net"
+            },
+            "author": null,
+            "title": "\"中 부동산 급락시 韓성장률 2% 내외…하반기 금리인하 논의\" - 한국무역협회",
+            "description": "한국무역협회 무역 통상정보, 회원/업무지원, 무역통계, 협회안내 등 서비스 안내.",
+            "url": "https://www.kita.net/board/totalTradeNews/totalTradeNewsDetail.do;JSESSIONID_KITA=B415ADCF44F05D0AF442C1B42B387853.Hyper?no=81889&amp;siteId=1",
+            "urlToImage": "https://kita.net/imgs/common/kita_logo_mid.png",
+            "publishedAt": "2024-02-15T00:56:15Z",
+            "content": ""
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "YouTube"
+            },
+            "author": null,
+            "title": "[세상만사] '손흥민·이강인' 충돌까지…클린스만 거취, 결론은? / 박문성 축구 해설위원 - YTN dmb",
+            "description": "아시안컵 이후,한국 축구계에 불어닥친 후폭풍이거셉니다.클린스만 감독을 경질할 것이냐 말 것이냐이 논란에 더해서,대회 기간 있었던손흥민, 이강인 선수의 갈등까지-수면 위로 떠올랐습니다.박문성 축구해설위원과 함께이야기 나눠봅니다. 위원님 어서오세요.YTN dmb 채널을 지금 바로 구독...",
+            "url": "https://www.youtube.com/watch?v=EARJRFXBquw",
+            "urlToImage": "https://i.ytimg.com/vi/EARJRFXBquw/maxresdefault.jpg",
+            "publishedAt": "2024-02-15T00:51:20Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Yna.co.kr"
+            },
+            "author": "김경렬",
+            "title": "신형 지대함 미사일 발사 장면 지켜 보는 김정은 - 연합뉴스",
+            "description": "(평양 조선중앙통신=연합뉴스) 김정은 북한 국무위원장은 지난 14일 오전 해군에 장비하게 되는 신형 지상대해상 미사일  '바다수리-6형'...",
+            "url": "https://www.yna.co.kr/view/PYH20240215037400042",
+            "urlToImage": "https://img5.yna.co.kr/photo/yna/YH/2024/02/15/PYH2024021503740004200_P4.jpg",
+            "publishedAt": "2024-02-15T00:48:16Z",
+            "content": ""
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Koreadaily.com"
+            },
+            "author": null,
+            "title": "이지아, 정계진출 막은 전남편 오민석에 감금..구하러 나선 강기영('끝내주는') - The Korea Daily",
+            "description": "[OSEN=강서정 기자] 이지아가 전남편 오민석의 정계 진출에 제대로 태클을 걸었다. 지난 14일 방송된 JTBC 수목드라마 ‘끝내주는 해결사’(극본 정희선, 연출 박진석) 5회에...",
+            "url": "https://news.koreadaily.com/2024/02/14/sports/broadcast/20240214162643447.html",
+            "urlToImage": "https://news.koreadaily.com/data/photo/2024/02/15/202402150922771353_65cd5a201ba1d.jpg",
+            "publishedAt": "2024-02-15T00:26:08Z",
+            "content": ""
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Munhwa.com"
+            },
+            "author": null,
+            "title": "임채무 “두리랜드 빚 190억, 3년새 30억 늘었지만…” - 문화일보",
+            "description": "돈없으면 집에 가서 빈대떡이나 부쳐 먹지...",
+            "url": "https://munhwa.com/news/view.html?no=20240215MW090936403624",
+            "urlToImage": "https://image.munhwa.com/gen_news/202402/20240215MW090936403624_b.jpg",
+            "publishedAt": "2024-02-15T00:09:00Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "YouTube"
+            },
+            "author": null,
+            "title": "뉴욕증시, 급락 하루 만에 반등‥나스닥 1.3%↑마감 (2024.02.15/뉴스투데이/MBC) - MBCNEWS",
+            "description": "뉴욕증시가 반등세를 나타냈습니다. 다우지수는 0.4% 올랐고, S&P500 지수와 나스닥 지수는 각각 0.9%, 1.3% 상승했습니다.https://imnews.imbc.com/replay/2024/nwtoday/article/6571324_36523.html#뉴욕증시, #국제유가...",
+            "url": "https://www.youtube.com/watch?v=mMmmmJr6HEw",
+            "urlToImage": "https://i.ytimg.com/vi/mMmmmJr6HEw/maxresdefault.jpg",
+            "publishedAt": "2024-02-14T23:19:45Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Hani.co.kr"
+            },
+            "author": "엄지원,강재구,이우연",
+            "title": "이재명 “떡잎 져야 새순 자란다” 대폭 물갈이 예고…임종석은? - 한겨레",
+            "description": "이재명 더불어민주당 대표가 14일 “떡잎이 져야 새순이 자란다”며 전면적인 ‘공천 물갈이’를 예고했다. 여론의 호응도가 높고 명분도 비교적 확실한, ‘올드 보이’들을 4월 총선에 내보내지 않겠다는 뜻은 평가해야 하지만, 결국 ‘비주류 솎아내기’로 귀결되는 게 아니냐는",
+            "url": "https://www.hani.co.kr/arti/politics/election/1128409.html",
+            "urlToImage": "https://flexible.img.hani.co.kr/flexible/normal/730/411/imgdb/original/2024/0214/20240214503715.jpg",
+            "publishedAt": "2024-02-14T22:40:00Z",
+            "content": null
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Socialfocus.co.kr"
+            },
+            "author": "연합뉴스",
+            "title": "황선우, 준결승 3위…자유형 100ｍ 한국 최초 세계선수권 결승행 - 소셜포커스",
+            "description": "한국 최초로 세계수영선수권 자유형 200ｍ 챔피언에 오른 황선우(20·강원도청)가 자유형 100ｍ에서도 한국 첫 메달 획득에 도전한다. 황선우는 15일(한국시간) 카타르 도하 어스파이어돔에서 열린 2024 국제수영연맹 세계선수권대회 경영 남자 자유형 100ｍ 준결승에서 47초93에 터치 패드를 찍어, 16명 중 3위로 결승(상위 8명)에 진출했다.자유형 100ｍ 결승은 16일 오전 1시 21분에 열린다. /연합뉴스",
+            "url": "http://www.socialfocus.co.kr/news/articleView.html?idxno=18619",
+            "urlToImage": "http://www.socialfocus.co.kr/image/logo/snslogo_20181023090945.jpg",
+            "publishedAt": "2024-02-14T21:55:26Z",
+            "content": ""
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Imbc.com"
+            },
+            "author": "남형석",
+            "title": "'북한 형제국' 쿠바와 수교‥\"중남미 외교 전환점\" - MBC 뉴스",
+            "description": "우리나라가 북한의 형제국으로 불리는 공산주의 국가 쿠바와 수교했습니다. 이로써 쿠바는 우리나라 193번째 수교국이 됐습니다. 남형석 기자입니다.  한국이 ...",
+            "url": "https://imnews.imbc.com/replay/2024/nwtoday/article/6571276_36523.html",
+            "urlToImage": "https://image.imnews.imbc.com/replay/2024/nwtoday/article/__icsFiles/afieldfile/2024/02/15/today_20240215_061000_1_6_Large.jpg",
+            "publishedAt": "2024-02-14T21:10:37Z",
+            "content": ""
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Chosun.com"
+            },
+            "author": "조선일보",
+            "title": "수하르토 독재 시절, 민주인사 탄압… 조코위, 국방장관 임명… 밀월 시작 - 조선일보",
+            "description": "수하르토 독재 시절, 민주인사 탄압 조코위, 국방장관 임명 밀월 시작 인니 새 대통령 확실시, 프라보워는 누구",
+            "url": "https://www.chosun.com/international/asia/2024/02/15/7GWCJD574JAD3NV7K77JXZMBRM/",
+            "urlToImage": "https://images.chosun.com/resizer/2mmOcQqqjs_p69YGJ573HqEPirY=/700x367/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/HUHPLCEFVBHMVMRSYKC4ICVQZA.jpg",
+            "publishedAt": "2024-02-14T19:12:00Z",
+            "content": null
+        }
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
